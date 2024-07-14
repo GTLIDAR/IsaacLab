9acbdcfcc741bff225d0a2d3286c14808fdf0ebf
WU Feiyang <fwu91@gatech.edu>
2024-07-14 16:35:08 -0400

    Relocate assets.

--- source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/digit.py
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/digit.py
@@ -14,6 +14,7 @@ import omni.isaac.lab.sim as sim_utils
 from omni.isaac.lab.actuators import ImplicitActuatorCfg
 from omni.isaac.lab.assets.articulation import ArticulationCfg
 from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
+from omni.isaac.lab_assets import ISAACLAB_ASSETS_DATA_DIR
 
 ##
 # Configuration
@@ -21,6 +22,7 @@ from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
 
 import os
 import torch
+
 torch.cuda.empty_cache()
 
 full_path = os.path.dirname(os.path.realpath(__file__))
@@ -28,7 +30,7 @@ full_path = os.path.dirname(os.path.realpath(__file__))
 
 DIGITV3_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{full_path}/../../../assets/robot/digit_v3_instanceable_2.usd", # digit_v3_july_3_with_value_range.usd
+        usd_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robot/Agility/digit/digit_v3_july_3_with_value_range.usd",
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
@@ -62,14 +64,14 @@ DIGITV3_CFG = ArticulationCfg(
             "left_toe_pitch": 0.13114588,
             "left_toe_roll": -0.01159121,
             "left_shoulder_roll": -1.50466737e-01,
-            "left_shoulder_pitch": 1.09051174e+00,
+            "left_shoulder_pitch": 1.09051174e00,
             "left_shoulder_yaw": 3.43707570e-04,
             "left_elbow": -1.39091311e-01,
             "right_hip_roll": -3.65734576e-01,
             "right_hip_yaw": 6.42881761e-03,
             "right_hip_pitch": -3.16910843e-01,
             "right_knee": -3.58016735e-01,
-            "right_shin":  0.01303884,
+            "right_shin": 0.01303884,
             "right_tarsus": 0.33119604,
             "right_heel_spring": 0.01160569,
             "right_toe_A": 1.32006717e-01,
@@ -77,7 +79,7 @@ DIGITV3_CFG = ArticulationCfg(
             "right_toe_pitch": -0.13114439,
             "right_toe_roll": 0.01117851,
             "right_shoulder_roll": 1.50437975e-01,
-            "right_shoulder_pitch": -1.09045901e+00,
+            "right_shoulder_pitch": -1.09045901e00,
             "right_shoulder_yaw": -3.51377474e-04,
             "right_elbow": 1.39086517e-01,
         },
@@ -98,14 +100,14 @@ DIGITV3_CFG = ArticulationCfg(
             effort_limit=200.0,
             velocity_limit=10.0,
             stiffness={
-                "left_hip_roll":100,
-                "left_hip_yaw":100,
-                "left_hip_pitch":200,
-                "left_knee":200,
-                "right_hip_roll":100,
-                "right_hip_yaw":100,
-                "right_hip_pitch":200,
-                "right_knee":200,
+                "left_hip_roll": 100,
+                "left_hip_yaw": 100,
+                "left_hip_pitch": 200,
+                "left_knee": 200,
+                "right_hip_roll": 100,
+                "right_hip_yaw": 100,
+                "right_hip_pitch": 200,
+                "right_knee": 200,
             },
             damping={
                 "left_hip_roll": 3.0,
@@ -162,10 +164,10 @@ DIGITV3_CFG = ArticulationCfg(
             effort_limit=200.0,
             velocity_limit=10.0,
             stiffness={
-                "left_toe_A":20,
-                "left_toe_B":20,
-                "right_toe_A":20,
-                "right_toe_B":20,
+                "left_toe_A": 20,
+                "left_toe_B": 20,
+                "right_toe_A": 20,
+                "right_toe_B": 20,
             },
             damping={
                 "left_toe_A": 1.0,
