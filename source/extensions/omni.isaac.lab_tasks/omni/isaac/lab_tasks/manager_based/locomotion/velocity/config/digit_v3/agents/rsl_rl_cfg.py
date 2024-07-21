efe71839816089494d540a1e6e9cbd809bf58dfb
xnal3 <xnal3@gatech.edu>
2024-07-20 22:45:45 -0400

    config gaussien noise, joints order in obs

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/agents/rsl_rl_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/agents/rsl_rl_cfg.py
@@ -39,7 +39,7 @@ class DigitV3RoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         desired_kl=0.01,
         max_grad_norm=1.0,
     )
-    logger = "wandb"
+    # logger = "wandb"
 
 
 @configclass
@@ -47,7 +47,7 @@ class DigitV3FlatPPORunnerCfg(DigitV3RoughPPORunnerCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.max_iterations = 4500
+        self.max_iterations = 6000
         self.experiment_name = "digit_v3_flat"
         self.policy.actor_hidden_dims = [128, 128, 128]
         self.policy.critic_hidden_dims = [128, 128, 128]
--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/rough_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/rough_env_cfg.py
@@ -13,6 +13,7 @@ from omni.isaac.lab.managers import ObservationGroupCfg as ObsGroup
 from omni.isaac.lab.managers import ObservationTermCfg as ObsTerm
 from omni.isaac.lab.envs import ManagerBasedEnv
 from omni.isaac.lab.utils.noise import AdditiveUniformNoiseCfg as Unoise
+from omni.isaac.lab.utils.noise import AdditiveGaussianNoiseCfg as Gnoise
 from omni.isaac.lab.utils import configclass
 
 import omni.isaac.lab_tasks.manager_based.locomotion.velocity.mdp as mdp
@@ -138,14 +139,9 @@ class ActionCfg:
             "right_shoulder_yaw",
             "right_elbow",
         ],
-        scale=0.5,
-        use_default_offset=True,
+        # scale=0.5,
+        # use_default_offset=True,
     )
-
-
-def constant_commands(env: ManagerBasedEnv) -> torch.Tensor:
-    """The generated command from the command generator."""
-    return torch.tensor([[1, 0, 0]], device=env.device).repeat(env.num_envs, 1)
 
 
 @configclass
@@ -157,15 +153,10 @@ class ObservationsCfg:
         """Observations for policy group."""
 
         # observation terms (order preserved)
-        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.1, n_max=0.1))
-        base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
-        # projected_gravity = ObsTerm(
-        #     func=mdp.projected_gravity,
-        #     noise=Unoise(n_min=-0.05, n_max=0.05),
-        # )
-        # velocity_commands = ObsTerm(func=constant_commands)
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Gnoise(mean=0.0, std=0.15, operation="add"))
+        base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Gnoise(mean=0.0, std=0.15, operation="add"))
         velocity_commands = ObsTerm(func=mdp.generated_commands, params={"command_name": "base_velocity"})
-        joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.01, n_max=0.01),
+        joint_pos = ObsTerm(func=mdp.joint_pos, # noise=Gnoise(mean=0.0, std=0.175, operation="add"),
                             params={
                                 "asset_cfg": SceneEntityCfg(
                                 "robot", joint_names=[
@@ -198,12 +189,14 @@ class ObservationsCfg:
                                                 "right_tarsus",
                                                 "right_toe_pitch",
                                                 "right_toe_roll",
-                                                "right_heel_spring",])
-                            })
+                                                "right_heel_spring"],
+                                            preserve_order = True
+                                                )
+                                        })
         
         
         
-        joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-1.5, n_max=1.5),
+        joint_vel = ObsTerm(func=mdp.joint_vel, #noise=Gnoise(mean=0.0, std=0.15, operation="add"),
                             params={
                                 "asset_cfg": SceneEntityCfg(
                                 "robot", joint_names=[
@@ -236,7 +229,9 @@ class ObservationsCfg:
                                                 "right_tarsus",
                                                 "right_toe_pitch",
                                                 "right_toe_roll",
-                                                "right_heel_spring",])})
+                                                "right_heel_spring",],
+                                            preserve_order = True)
+                                    })
         actions = ObsTerm(func=mdp.last_action)
 
 
@@ -257,31 +252,53 @@ class DigitV3RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
     actions: ActionCfg = ActionCfg()
     observations: ObservationsCfg = ObservationsCfg()
 
+
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
+        self.sim.dt = 0.001
+        self.decimation = 20
         # Scene
         self.scene.robot = DIGITV3_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
         self.scene.height_scanner.prim_path = "{ENV_REGEX_NS}/Robot/base"
 
         # Randomization
         self.events.push_robot = None
-        self.events.add_base_mass = None
+        # self.events.add_base_mass = None
+
+        self.events.add_base_mass.params["asset_cfg"].body_names = [
+            ".*base"
+        ]
         self.events.reset_robot_joints.params["position_range"] = (1.0, 1.0)
         self.events.base_external_force_torque.params["asset_cfg"].body_names = [
             ".*base"
         ]
-        self.events.reset_base.params = {
-            "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-3.14, 3.14)},
-            "velocity_range": {
-                "x": (0.0, 0.0),
-                "y": (0.0, 0.0),
-                "z": (0.0, 0.0),
-                "roll": (0.0, 0.0),
-                "pitch": (0.0, 0.0),
-                "yaw": (0.0, 0.0),
-            },
-        }
+
+        # self.events.reset_base.params = {
+        #     "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-3.14, 3.14)},
+        #     "velocity_range": {
+        #         "x": (-0.5, 0.5),
+        #         "y": (-0.5, 0.5),
+        #         "z": (-0.5, 0.5),
+        #         "roll": (-0.1, 0.1),
+        #         "pitch": (-0.1, 0.1),
+        #         "yaw": (-0.1, 0.1),
+        #     },
+        # }
+
+        self.events.reset_base = None
+
+        # self.events.reset_base.params = {
+        #     "pose_range": {"x": (-0.0, 0.0), "y": (-0.0, 0.0), "yaw": (-0, 0)},
+        #     "velocity_range": {
+        #         "x": (0.0, 0.0),
+        #         "y": (0.0, 0.0),
+        #         "z": (0.0,0.0),
+        #         "roll": (0.0, 0.0),
+        #         "pitch": (0.0, 0.0),
+        #         "yaw": (0.0, 0.0),
+        #     },
+        # }
 
         # Terminations
         self.terminations.base_contact.params["sensor_cfg"].body_names = [".*base", ".*hip.*",
@@ -316,6 +333,7 @@ class DigitV3RoughEnvCfg_PLAY(DigitV3RoughEnvCfg):
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
+        
 
         # make a smaller scene for play
         self.scene.num_envs = 50
