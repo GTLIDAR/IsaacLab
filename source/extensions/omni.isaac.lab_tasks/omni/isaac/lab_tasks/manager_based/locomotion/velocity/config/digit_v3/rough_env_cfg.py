c48f8fdbb8850940fd83016a7a4f1f86efa521fe
xnal3 <xnal3@gatech.edu>
2024-08-15 20:17:08 -0400

    clean obs

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/rough_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/rough_env_cfg.py
@@ -240,50 +240,6 @@ class ObservationsCfg:
                                     })
         actions = ObsTerm(func=mdp.last_action)
 
-        joint_vel = ObsTerm(
-            func=mdp.joint_vel,
-            noise=Gnoise(mean=0.0, std=0.05, operation="add"),
-            params={
-                "asset_cfg": SceneEntityCfg(
-                    "robot",
-                    joint_names=[
-                        "left_hip_roll",
-                        "left_hip_yaw",
-                        "left_hip_pitch",
-                        "left_knee",
-                        "left_toe_A",
-                        "left_toe_B",
-                        "right_hip_roll",
-                        "right_hip_yaw",
-                        "right_hip_pitch",
-                        "right_knee",
-                        "right_toe_A",
-                        "right_toe_B",
-                        "left_shoulder_roll",
-                        "left_shoulder_pitch",
-                        "left_shoulder_yaw",
-                        "left_elbow",
-                        "right_shoulder_roll",
-                        "right_shoulder_pitch",
-                        "right_shoulder_yaw",
-                        "right_elbow",
-                        "left_shin",
-                        "left_tarsus",
-                        "left_toe_pitch",
-                        "left_toe_roll",
-                        "left_heel_spring",
-                        "right_shin",
-                        "right_tarsus",
-                        "right_toe_pitch",
-                        "right_toe_roll",
-                        "right_heel_spring",
-                    ],
-                    preserve_order=True,
-                )
-            },
-        )
-        actions = ObsTerm(func=mdp.last_action)
-
         def __post_init__(self):
             self.enable_corruption = True
             self.concatenate_terms = True
