ce2f283a297c29b8f47d16d42cef496ed3448900
xnal3 <xnal3@gatech.edu>
2024-08-26 18:23:55 -0400

    clean code

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/rough_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/digit_v3/rough_env_cfg.py
@@ -6,6 +6,7 @@
 import torch
 
 from omni.isaac.lab.managers import RewardTermCfg as RewTerm
+from omni.isaac.lab.managers import EventTermCfg as EventTerm
 from omni.isaac.lab.managers import SceneEntityCfg
 from omni.isaac.lab.managers import TerminationTermCfg as DoneTerm
 from omni.isaac.lab.managers import ObservationGroupCfg as ObsGroup
@@ -19,6 +20,7 @@ import omni.isaac.lab_tasks.manager_based.locomotion.velocity.mdp as mdp
 from omni.isaac.lab_tasks.manager_based.locomotion.velocity.velocity_env_cfg import (
     LocomotionVelocityRoughEnvCfg,
     RewardsCfg,
+    EventCfg
 )
 
 
@@ -29,7 +31,95 @@ from omni.isaac.lab_assets.digit import DIGITV3_CFG  # isort: skip
 
 
 @configclass
-class DigitV3Rewards(RewardsCfg):
+class DigitV3EventCfg(EventCfg):
+    # startup
+    physics_material = EventTerm(
+        func=mdp.randomize_rigid_body_material,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
+            "static_friction_range": (0.7, 1.3),
+            "dynamic_friction_range": (0.5, 1.0),
+            "restitution_range": (0.0, 0.0),
+            "num_buckets": 64,
+        },
+    )
+
+    # reset
+    add_base_mass = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="reset",
+        params={
+            "asset_cfg":SceneEntityCfg("robot", body_names=".*"),
+            "mass_distribution_params": (0.5, 1.5),
+            "operation": "scale",
+            "distribution": "uniform",
+        },
+    )
+
+    reset_gravity = EventTerm(
+        func=mdp.randomize_physics_scene_gravity,
+        mode="reset",
+        # is_global_time=True,
+        # interval_range_s=(0.0, 36.0),  # time_s = num_steps * (decimation * dt)
+        params={
+            "gravity_distribution_params": ([0.0, 0.0, 0.0], [0.0, 0.0, 0.4]),
+            "operation": "add",
+            "distribution": "gaussian",
+        },
+    )
+
+    
+    reset_base = EventTerm(
+        func=mdp.reset_root_state_uniform,
+        mode="reset",
+        params={
+            "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-0.5, -0.5)},
+            "velocity_range": {
+                "x": (-0.0, 0.0),
+                "y": (-0.0, 0.0),
+                "z": (-0.0, 0.0),
+                "roll": (-0.0, 0.0),
+                "pitch": (-0.0, 0.0),
+                "yaw": (-0.0, 0.0),
+            },
+        },
+    )
+
+    reset_robot_joints = EventTerm(
+        func=mdp.reset_joints_by_scale,
+        mode="reset",
+        params={
+            "position_range": (0.9, 1.1),
+            "velocity_range": (0.0, 0.0),
+        },
+    )
+
+    #interval
+    base_external_force_torque = EventTerm(
+        func=mdp.apply_external_force_torque,
+        mode="interval",
+        interval_range_s=(0.0, 20.0),
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names="base"),
+            "force_range": (0.0, 5.0),
+            "torque_range": (-5.0, 5.0),
+        },
+    )
+
+
+
+    push_robot = EventTerm(
+        func=mdp.push_by_setting_velocity,
+        mode="interval",
+        interval_range_s=(0.0, 20.0),
+        params={"velocity_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5),"z": (-0.1, 0.5),}},
+    )
+
+
+
+@configclass
+class DigitV3RewardsCfg(RewardsCfg):
     termination_penalty = RewTerm(func=mdp.is_terminated, weight=-200.0)  # type: ignore
 
     alive = RewTerm(
@@ -108,21 +198,25 @@ class DigitV3Rewards(RewardsCfg):
 
 
 @configclass
-class TerminationsCfg:
+class DigitV3TerminationsCfg:
     """Termination terms for the MDP."""
 
     time_out = DoneTerm(func=mdp.time_out, time_out=True)  # type: ignore
     base_contact = DoneTerm(
         func=mdp.illegal_contact,  # type: ignore
         params={
-            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*base"),
+            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=[
+                        ".*base",
+                        ".*hip.*",
+                        ".*knee",
+                        ".*elbow",]),
             "threshold": 10.0,
         },
     )
 
 
 @configclass
-class ActionCfg:
+class DigitV3ActionCfg:
     """Action terms for the MDP."""
 
     joint_pos = mdp.JointPositionActionCfg(  # type: ignore
@@ -156,7 +250,7 @@ class ActionCfg:
 
 
 @configclass
-class ObservationsCfg:
+class DigitV3ObservationsCfg:
     """Observation specifications for the MDP."""
 
     @configclass
@@ -255,10 +349,14 @@ class ObservationsCfg:
 
 @configclass
 class DigitV3RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
-    rewards: DigitV3Rewards = DigitV3Rewards()
-    terminations: TerminationsCfg = TerminationsCfg()
-    actions: ActionCfg = ActionCfg()
-    observations: ObservationsCfg = ObservationsCfg()
+    # Basic settings
+    observations: DigitV3ObservationsCfg = DigitV3ObservationsCfg()
+    actions: DigitV3ActionCfg = DigitV3ActionCfg()
+
+    #MDP settings
+    rewards: DigitV3RewardsCfg = DigitV3RewardsCfg()
+    terminations: DigitV3TerminationsCfg = DigitV3TerminationsCfg()
+    events: DigitV3EventCfg = DigitV3EventCfg()
 
     def __post_init__(self):
         # post init of parent
@@ -272,43 +370,9 @@ class DigitV3RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         # Scene
         self.scene.robot = DIGITV3_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
         self.scene.height_scanner.prim_path = "{ENV_REGEX_NS}/Robot/base"
-
-        # Randomization
-      
-
-        self.events.add_base_mass.params["asset_cfg"].body_names = [
-            ".*base"
-            ".*"
-        ]
-        
-        self.events.base_external_force_torque.params["force_range"] = (0.0, 5.0)
-        self.events.base_external_force_torque.params["torque_range"] = (-5.0, 5.0)
+   
 
 
-        self.events.reset_base.params = {
-            "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-0.5, 0.5)},
-            "velocity_range": {
-                "x": (-0.0, 0.0),
-                "y": (-0.0, 0.0),
-                "z": (-0.0, 0.0),
-                "roll": (-0.0, 0.0),
-                "pitch": (-0.0, 0.0),
-                "yaw": (-0.0, 0.0),
-            },
-        }
-
-        self.events.reset_robot_joints.params["position_range"] = (0.9, 1.1)
-        self.events.reset_robot_joints.params["velocity_range"] = (0.0, 0.0)
-        
-        # self.events.push_robot = None
-
-        # Terminations
-        self.terminations.base_contact.params["sensor_cfg"].body_names = [
-            ".*base",
-            ".*hip.*",
-            ".*knee",
-            ".*elbow",
-        ]
 
         # Rewards
         self.rewards.dof_acc_l2.params["asset_cfg"] = SceneEntityCfg(
