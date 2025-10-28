3baf9ce93f07c37621252a5bec057c7c0cda90ae
yangchen73 <122090643@link.cuhk.edu.cn>
2025-10-28 23:18:49 +0800

    add some obs to the teacher

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/digit_v3/env_cfg/observation_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/digit_v3/env_cfg/observation_cfg.py
@@ -13,7 +13,6 @@ import isaaclab_tasks.manager_based.locomotion.velocity.config.digit_v3.mdp as d
 class TeacherObsCfg(ObsGroup):
     """Observations for policy group."""
 
-    # observation terms (order preserved)
     clock = ObsTerm(
         func=digit_mdp.clock,
     )
@@ -33,6 +32,118 @@ class TeacherObsCfg(ObsGroup):
     joint_vel = ObsTerm(func=mdp.joint_vel)
 
     actions = ObsTerm(func=mdp.last_action)
+
+    root_state_w = ObsTerm(func=digit_mdp.root_state_w)
+
+    root_lin_vel = ObsTerm(func=mdp.root_lin_vel_w)
+
+    root_ang_vel = ObsTerm(func=mdp.root_ang_vel_w)
+
+    root_pos = ObsTerm(func=mdp.root_pos_w)
+
+    root_quat = ObsTerm(func=mdp.root_quat_w)
+    # student specific observations
+    student_base_lin_vel = ObsTerm(
+        func=mdp.base_lin_vel,
+        scale=1,
+    )
+    student_base_ang_vel = ObsTerm(
+        func=mdp.base_ang_vel,
+        scale=1,
+    )
+    student_projected_gravity = ObsTerm(
+        func=mdp.projected_gravity,
+    )
+    student_velocity_commands = ObsTerm(
+        func=mdp.generated_commands,
+        scale=1,
+        params={"command_name": "base_velocity"},
+    )
+    student_joint_pos = ObsTerm(
+        func=mdp.joint_pos,
+        scale=1,
+        params={
+            "asset_cfg": SceneEntityCfg(
+                "robot",
+                joint_names=[
+                    "left_hip_roll",
+                    "left_hip_yaw",
+                    "left_hip_pitch",
+                    "left_knee",
+                    "left_toe_A",
+                    "left_toe_B",
+                    "right_hip_roll",
+                    "right_hip_yaw",
+                    "right_hip_pitch",
+                    "right_knee",
+                    "right_toe_A",
+                    "right_toe_B",
+                    "left_shoulder_roll",
+                    "left_shoulder_pitch",
+                    "left_shoulder_yaw",
+                    "left_elbow",
+                    "right_shoulder_roll",
+                    "right_shoulder_pitch",
+                    "right_shoulder_yaw",
+                    "right_elbow",
+                    "left_shin",
+                    "left_tarsus",
+                    "left_toe_pitch",
+                    "left_toe_roll",
+                    "left_heel_spring",
+                    "right_shin",
+                    "right_tarsus",
+                    "right_toe_pitch",
+                    "right_toe_roll",
+                    "right_heel_spring",
+                ],
+                preserve_order=True,
+            )
+        },
+    )
+
+    student_joint_vel = ObsTerm(
+        func=mdp.joint_vel,
+        scale=1,
+        params={
+            "asset_cfg": SceneEntityCfg(
+                "robot",
+                joint_names=[
+                    "left_hip_roll",
+                    "left_hip_yaw",
+                    "left_hip_pitch",
+                    "left_knee",
+                    "left_toe_A",
+                    "left_toe_B",
+                    "right_hip_roll",
+                    "right_hip_yaw",
+                    "right_hip_pitch",
+                    "right_knee",
+                    "right_toe_A",
+                    "right_toe_B",
+                    "left_shoulder_roll",
+                    "left_shoulder_pitch",
+                    "left_shoulder_yaw",
+                    "left_elbow",
+                    "right_shoulder_roll",
+                    "right_shoulder_pitch",
+                    "right_shoulder_yaw",
+                    "right_elbow",
+                    "left_shin",
+                    "left_tarsus",
+                    "left_toe_pitch",
+                    "left_toe_roll",
+                    "left_heel_spring",
+                    "right_shin",
+                    "right_tarsus",
+                    "right_toe_pitch",
+                    "right_toe_roll",
+                    "right_heel_spring",
+                ],
+                preserve_order=True,
+            )
+        },
+    )
 
     height_scan = ObsTerm(
         func=mdp.height_scan,
